5a2305e66cfcdd485a71307184f9a53ffa09313d
Sina Booeshaghi <sbooeshaghi@gmail.com>
2021-10-01 17:29:54 -0700

    init

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+/*
+	Photon by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1140px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)',
+		xxsmall: '(max-width: 320px)'
+	});
+
+	$(function() {
+
+
+			skel.on('+mobile -mobile', function() {
+				$.prioritize(
+					'.important\\28 mobile\\29',
+					skel.breakpoint('mobile').active
+				);
+			});
+
+			$('.scrolly').scrolly();
+
+	});
+
+})(jQuery);
